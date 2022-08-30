479d0602938723f3215df12ca0967115a2d7d5a0
Melk12345 <105999204+Melk12345@users.noreply.github.com>
2022-08-29 21:29:34 -0400

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -59,11 +59,11 @@ const upgrades = [{
 }, {
     description: "Unlocks the Boost Autobuyer button!",
     type: "Unlockable",
-    unlockCost: 1e18
+    unlockCost: 1e15
 }, {
     description: "Unlocks the Upgrades Autobuyer button!",
     type: "Unlockable",
-    unlockCost: 1e23
+    unlockCost: 1e17
 }]
 
 let data = {
